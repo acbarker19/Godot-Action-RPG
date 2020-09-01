c726b45caa6d352696bccc304a4647649b4628d8
Alec Barker <acbarker19@gmail.com>
2020-09-01 17:36:09 -0400

    Initial Player Movement

---
+++ Player/Player.gd
@@ -0,0 +1,21 @@
+extends KinematicBody2D
+
+const ACCELERATION = 20
+const MAX_SPEED = 150
+const FRICTION = 25
+
+var velocity = Vector2.ZERO
+
+func _physics_process(delta):
+	var input_vector = Vector2.ZERO
+	input_vector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+	input_vector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+	input_vector = input_vector.normalized()
+	
+	if input_vector != Vector2.ZERO:
+		velocity += input_vector * ACCELERATION * delta
+		velocity = velocity.clamped(MAX_SPEED * delta)
+	else:
+		velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+
+	move_and_collide(velocity)
